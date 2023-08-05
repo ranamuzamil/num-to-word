3dda5a573305ea61b1657e5333396666a3d152ef
rm muzamil <ramuzamil44@gmail.com>
2023-08-06 00:45:08 +0500

    1.1

--- script.js
+++ script.js
@@ -1,31 +1,50 @@
+let display = document.getElementById("display");
 function numToWord(num) {
     let onceArr = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sisteen", "seventeen", "eighteen", "nineteen", "twenty"];
     let after20 = ["zero", "ten", "twenty", "thrty", "forty", "fifty", "sisty", "seventy", "eighty", "ninty"]
-    let millions = ["", "", "", "hundered", "thousand", "million", "billion", "trillion", "quadrillion", "quintrillion", "sextillion", "septillion", "octillion", "nonillion", "decillion"]
-
 
     let ans = '';
-    if (num >= 100000) {                                      // In case of lakh
-        //  if (num >= 11000) {
-        //     ans += onceArr[Math.floor(num / 1000)]
-        // num %= 1000;
-        // } 
+    if (num < 1) {
+        ans = onceArr[num]  
+    }
+
+    if (num >= 1000000000) {                                    // In case of arab
+        if (num >= 21000000000) {
+            ans += after20[Math.floor(num / 10000000000)] + " "
+            num %= 10000000000;
+        }
+        if (num >= 1000000000) {
+            ans += onceArr[Math.floor(num / 1000000000)]
+            num %= 1000000000;
+        }
+        ans += " arab "
+    }
+    if (num >= 10000000) {                                    // In case of crore's
+        if (num >= 210000000) {
+            ans += after20[Math.floor(num / 100000000)] + " "
+            num %= 100000000;
+        }
+        if (num >= 10000000) {
+            ans += onceArr[Math.floor(num / 10000000)]
+            num %= 10000000;
+        }
+        ans += " crore "
+    }
+    if (num >= 100000) {                                      // In case of lakh's
+        if (num >= 2100000) {
+            ans += after20[Math.floor(num / 1000000)] + " "
+            num %= 1000000;
+        }
         if (num >= 100000) {
             ans += onceArr[Math.floor(num / 100000)]
             num %= 100000;
         }
         ans += " lakh "
     }
-    if (num >= 1000) {                                        // in case of thousands
+    if (num >= 1000) {                                        // in case of thousand's
 
         if (num >= 21000) {
             ans += after20[Math.floor(num / 10000)] + " "
-            num %= 10000;
-        } if (num >= 11000) {
-            ans += onceArr[Math.floor(num / 1000)]
-            num %= 1000;
-        } if (num >= 10000) {
-            ans += after20[Math.floor(num / 10000)]
             num %= 10000;
         } if (num >= 1000) {
             ans += onceArr[Math.floor(num / 1000)]
@@ -33,7 +52,7 @@ function numToWord(num) {
         }
         ans += " thousand "
 
-    } if (num >= 100) {                                       // In case of hundereds
+    } if (num >= 100) {                                       // In case of hundered's
         ans += onceArr[Math.floor(num / 100)] + " hundered "
         num %= 100;
     } if (num >= 20) {
@@ -44,4 +63,7 @@ function numToWord(num) {
     }
     return ans;
 }
-console.log(numToWord(21001));
+function myFunction() {
+    let userData = document.getElementById("myInput").value;
+    display.textContent = numToWord(userData)
+}
